63fb2d57160a34270ebd24205efff4edd381e78a
Henry Lima <djfilipebr@gmail.com>
2026-03-24 21:07:10 -0300

    Future: Add createClipboardWindow function to manage clipboard window creation

---
+++ electron/window/clipboardWindow.ts
@@ -0,0 +1,30 @@
+/* import { BrowserWindow } from 'electron'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import { clipboardWindow, RENDERER_DIST, VITE_DEV_SERVER_URL } from '../main'
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+export function createClipboardWindow() {
+  if (clipboardWindow) return clipboardWindow
+
+  const __clipboardWindow = new BrowserWindow({
+    width: 400,
+    height: 200,
+    frame: false,
+    resizable: false,
+    title: 'Área de Transferência Inteligente',
+    icon: path.join(process.env.VITE_PUBLIC, 'icon.png'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.mjs'),
+    },
+  })
+
+  if(VITE_DEV_SERVER_URL) {
+    __clipboardWindow.loadURL(`${VITE_DEV_SERVER_URL}/clipboard`)
+  } else {
+    __clipboardWindow.loadFile(path.join(RENDERER_DIST, 'index.html'))
+  }
+
+  return __clipboardWindow
+} */
